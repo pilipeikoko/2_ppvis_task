f12ed1e86c69187f144b07a7334611aefc48a0cb
pilipeikoko <pilipeikoko@gmail.com>
2021-06-27 18:14:55 +0300

    added linked list to repository

--- src/main/java/org/bsuir/task/repository/GroupRepository.java
+++ src/main/java/org/bsuir/task/repository/GroupRepository.java
@@ -1,33 +1,76 @@
 package org.bsuir.task.repository;
 
 import org.bsuir.task.entity.Group;
-import org.bsuir.task.entity.impl.GroupImpl;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class GroupRepository {
     //Thread safe singleton with Initialization On-Demand Holder
     //source: https://avaldes.com/creating-a-thread-safe-singleton-class-with-examples/
-    private final List<GroupImpl> groupImpls;
+    private final Map<Integer, LinkedList<Group>> storage;
 
     private static class Holder {
         private static final GroupRepository instance = new GroupRepository();
     }
 
-    private GroupRepository(){
-        groupImpls = new ArrayList<>();
+    private GroupRepository() {
+        storage = new HashMap<>();
     }
 
     public static GroupRepository getInstance() {
         return Holder.instance;
     }
 
+    //fixme
+    //no, i won't use streams, diy
     public List<Group> getGroups() {
-        return List.copyOf(groupImpls);
+        Map<Integer, LinkedList<Group>> copy = Map.copyOf(storage);
+        List<Group> result = new ArrayList<>();
+
+        for (LinkedList<Group> list : copy.values()) {
+            result.addAll(list);
+        }
+
+        return result;
+    }
+
+    public List<Group> getGroupsByCourse(int courseNumber) {
+        Map<Integer, LinkedList<Group>> copy = Map.copyOf(storage);
+
+        if (copy.containsKey(courseNumber)) {
+            List<Integer> keys = new ArrayList<>(copy.keySet());
+
+            for (Integer integer : keys) {
+                if (integer == courseNumber) {
+                    return copy.get(integer);
+                }
+            }
+        }
+
+        return null;
     }
 
     public boolean removeGroup(Group groupToRemove) {
-        return groupImpls.remove(groupToRemove);
+        for (LinkedList<Group> list : storage.values()) {
+            if (list.contains(groupToRemove)) {
+                return list.remove(groupToRemove);
+            }
+        }
+        return false;
+    }
+
+    public boolean addGroup(Group group) {
+        int courseNumber = group.getCourseNumber();
+
+        if (storage.containsKey(courseNumber)) {
+            List<Integer> keys = new ArrayList<>(storage.keySet());
+
+            for (Integer integer : keys) {
+                if (integer == courseNumber) {
+                    return storage.get(integer).add(group);
+                }
+            }
+        }
+        return false;
     }
 }
